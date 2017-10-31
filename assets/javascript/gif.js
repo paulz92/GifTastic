1e17b56157b13c6c032fac5522262f39f647951f
Paul Zavattieri <paulz92@gmail.com>
2017-10-31 16:43:23 -0400

    added api/ajax calls and functionality

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -6,17 +6,19 @@ $(document).ready(function() {
 		emotions: ["happy", "sad", "scared", "angry",
 			 "surprised", "confused", "awkward", "drunk", 
 			 "excited", "bored"],
-		// function for user added buttons
+		// method for user added buttons
 		addEmotions: function() {
 			// setting variable equal to user input text and error
-			// handling any leading/trailing spaces from user
-			var newEmotion = $("#emotion-input").val().trim();
+			// handling any leading/trailing spaces from user and
+			// putting it all to lowercase
+			var newEmotion = $("#emotion-input").val().trim()
+				.toLowerCase();
 			// pushing the new emotion into emotion array
 			gifGenerator.emotions.push(newEmotion);
-			// run the display emotions function
+			// run the display emotions meethod
 			gifGenerator.displayButtons();
 		},
-		// function to display the buttons
+		// method to display the buttons
 		displayButtons: function () {
 			// empty the buttons div each time this runs so that it 
 			// doesn't re-write all the buttons over each time user 
@@ -28,18 +30,65 @@ $(document).ready(function() {
 				var button = $("<button>");
 				// give it bootstrap button classes and emotion class
 				button.addClass("btn btn-primary emotion");
+				// give it a data name equal to the emotion
+				button.attr("data-name", gifGenerator.emotions[i]);
 				// give it text equal to emotion in current array index
 				button.text(gifGenerator.emotions[i]);
 				// append button to the buttons div
 				$("#emotion-buttons").append(button);
 			}
 		},
-		// displayGifs: function() {
+		// method to display gifs
+		displayGifs: function() {
+			// clears prior gif each time a new button is clicked
+			$("#emotion-gifs").empty();
+			// beta api key from giphy
+			var apiKey = "005nfNXY5tfl1ZFiBX7GH8Hzg9aWf6sQ";
+			// data name of the button clicked will be search parameter
+			var clickEmotion = $(this).attr("data-name");
+			// number of gifs to display
+			var numGifs = 10;
+			// max rating pg
+			var rating = "pg"
+			// buildin query url
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        clickEmotion + "&rating=" + rating + "&api_key=" + apiKey
+        + "&limit=" + numGifs;
 
-		// },
-		// animateGifs: function() {
-
-		// }
+      // ajax call getting JSON object from query url
+      $.ajax({
+      	url: queryURL,
+      	method: "GET"
+      })
+      // after data comes back from request
+      .done(function(response) {
+      	// saving response.data in var for shorthand
+      	var results = response.data;
+      	// loop through the # of data results in response
+      	for (var i = 0; i < results.length; i++) {
+      		// creating div for info to go into
+      		var gifDiv = $("<div>");
+      		// adding gif class for css styling/click feature
+      		gifDiv
+      			.addClass("gif");
+      		// creating <p> for the rating info	
+      		var p = $("<p>").text("Rating: " + results[i].rating);
+      		// creating image tag for the gif
+      		var gif = $("<img>");
+      		// giving image tag source with gif url at i
+      		gif.attr("src", results[i].images.fixed_height.url);
+      		// appending the p and image to the gifDiv
+      		gifDiv.append(p);
+      		gifDiv.append(gif);
+      		// prepending the giv div to the emotion gifs row/div
+      		$("#emotion-gifs").prepend(gifDiv);
+      	}
+      });
+		},
+		// method to animate gifs
+		animateGifs: function() {
+			console.log("placehold");
+		}
 	// close object
 	};
 
@@ -53,13 +102,13 @@ $(document).ready(function() {
 		gifGenerator.addEmotions();
 	});
 
-	// // click event listener to all elements with "emotion" class
-	// // will run the diplay Gifs method
- //  $(document).on("click", "emotion", displayGifs);
+// click event listener to all elements with "emotion" class
+// will run the diplay Gifs method
+ $(document).on("click", ".emotion", gifGenerator.displayGifs);
 
-	// // click event listener to all elements with "gif" class
-	// // will run the animate Gifs method
-	// $(document).on("click", "gif", animateGifs);
+	// click event listener to all elements with "gif" class
+	// will run the animate Gifs method
+	$(document).on("click", ".gif", gifGenerator.animateGifs);
 
 // close ready function
 });
